13c7a623f7c5dc21a64177876a00045814cad722
vvduth <ducthai060501@gmail.com>
2025-04-01 16:22:09 +0300

    Add authentication pages and enhance TopNav with navigation links

--- components/Navbar/TopNav.tsx
+++ components/Navbar/TopNav.tsx
@@ -1,39 +1,61 @@
-import {GiMatchTip} from 'react-icons/gi';
- import {Navbar, NavbarBrand, NavbarContent} from '@heroui/navbar';
- import Link from 'next/link';
- import {Button} from '@heroui/button';
-
+import { GiMatchTip } from "react-icons/gi";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/navbar";
+import Link from "next/link";
+import { Button } from "@heroui/button";
 
 const TopNav = () => {
   return (
-    <Navbar maxWidth={'xl'}
-            className="bg-gradient-to-r from-purple-400 to-purple-700"
-            classNames={{
-                item: [
-                    'text-xl',
-                    'text-white',
-                    'uppercase',
-                    'data-[active=true]:text-yellow-200'
-                ]
-            }}
+    <Navbar
+      maxWidth={"xl"}
+      className="bg-gradient-to-r from-purple-400 to-purple-700"
+      classNames={{
+        item: [
+          "text-xl",
+          "text-white",
+          "uppercase",
+          "data-[active=true]:text-yellow-200",
+        ],
+      }}
     >
-        <NavbarBrand as={Link} href='/'>
-            <GiMatchTip size={40} className="text-gray-200" />
-            <div className='font-bold text-3xl flex'>
-                <span className='text-gray-900'>Next</span>
-                <span className='text-gray-200'>Match</span>
-            </div>
-        </NavbarBrand>
-        <NavbarContent justify="center">
-            
-        </NavbarContent>
-        <NavbarContent justify="end">
-            <Button as={Link} href='/login' variant='bordered' className={'text-white'}>Login</Button>
-            <Button as={Link} href='/register' variant='bordered' className={'text-white'}>Register</Button>
-        </NavbarContent>
+      <NavbarBrand as={Link} href="/">
+        <GiMatchTip size={40} className="text-gray-200" />
+        <div className="font-bold text-3xl flex">
+          <span className="text-gray-900">Next</span>
+          <span className="text-gray-200">Match</span>
+        </div>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem as={Link} href={"/members"}>
+          Matches
+        </NavbarItem>
+        <NavbarItem as={Link} href={"/lists"}>
+          Lists
+        </NavbarItem>
+
+        <NavbarItem as={Link} href={"/messages"}>
+          Messages
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <Button
+          as={Link}
+          href="/login"
+          variant="bordered"
+          className={"text-white"}
+        >
+          Login
+        </Button>
+        <Button
+          as={Link}
+          href="/register"
+          variant="bordered"
+          className={"text-white"}
+        >
+          Register
+        </Button>
+      </NavbarContent>
     </Navbar>
-);
-  
+  );
 };
 
 export default TopNav;
