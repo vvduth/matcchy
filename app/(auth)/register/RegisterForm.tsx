390ce4159b37ad19b55ed0cda0e2e1051dc47d97
vvduth <ducthai060501@gmail.com>
2025-04-02 10:45:40 +0300

    Refactor registration logic to handle server errors and improve user feedback; update RegisterForm component with loading state and error messages; introduce ActionResult type for consistent API responses

--- app/(auth)/register/RegisterForm.tsx
+++ app/(auth)/register/RegisterForm.tsx
@@ -1,83 +1,100 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 import { Button, Card, CardBody, CardHeader, Input } from "@heroui/react";
 
 import { GiPadlock } from "react-icons/gi";
 import { useForm } from "react-hook-form";
-import { RegisterSchema, registerSchema } from '@/lib/registerSchema';
+import { RegisterSchema, registerSchema } from "@/lib/registerSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { registerUser } from '@/app/actions/authActions';
+import { registerUser } from "@/app/actions/authActions";
 const RegisterForm = () => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors, isValid },
-      } = useForm<RegisterSchema>({
-        resolver: zodResolver(registerSchema),
-        mode: 'onTouched'
-      });
-      const onSubmit = async (data: RegisterSchema) => {
-        console.log('trigerred');
-        const result = await registerUser(data)
-        console.log(result)
-      };
-      return (
-        <Card className="w-2/5 mx-auto ">
-          <CardHeader
-            className="flex flex-col items-center
+  const {
+    register,
+    handleSubmit,
+    
+    setError,
+    
+    formState: { errors, isValid, isSubmitting },
+  } = useForm<RegisterSchema>({
+    resolver: zodResolver(registerSchema),
+    mode: "onTouched",
+  });
+  const onSubmit = async (data: RegisterSchema) => {
+    const result = await registerUser(data);
+    if (result.status === "success") {
+      console.log("sucess");
+    } else {
+      if (Array.isArray(result.error)) {
+        result.error.forEach((e: any) => {
+          const fieldName = e.path.join("");
+          setError(fieldName, { message: e.message });
+        });
+      } else {
+        setError("root.serverError", { message: result.error });
+      }
+    }
+  };
+  return (
+    <Card className="w-2/5 mx-auto ">
+      <CardHeader
+        className="flex flex-col items-center
            justify-center"
-          >
-            <div className="flex flex-col gap-2 items-center text-secondary">
-              <div className="flex flex-row items-center gap-3">
-                <GiPadlock size={30} />
-                <h1 className="text-3xl font-semibold">Register</h1>
-              </div>
-              <p className="text-neutral-500">Welcome to Matchhy</p>
-            </div>
-          </CardHeader>
-          <CardBody>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="space-y-4">
-                <Input
-                  defaultValue=""
-                  label="Email"
-                  variant="bordered"
-                  {...register("email")}
-                  isInvalid={!!errors.email}
-                  errorMessage={errors.email?.message as string}
-                />
-                <Input
-                  defaultValue=""
-                  label="Name"
-                  variant="bordered"
-                  {...register("name")}
-                  isInvalid={!!errors.name}
-                  errorMessage={errors.name?.message as string}
-                />
-                <Input
-                  defaultValue=""
-                  label="Password"
-                  type="password"
-                  variant="bordered"
-                  {...register("password")}
-                  isInvalid={!!errors.password}
-                  errorMessage={errors.password?.message as string}
-                />
-                <Button
-                  disabled={!isValid}
-                  fullWidth
-                  color="secondary"
-                  type="submit"
-                >
-                  {isValid ? (<>Register</>) : (
-                    <>No no</>
-                  )}
-                </Button>
-              </div>
-            </form>
-          </CardBody>
-        </Card>
-      );
-}
+      >
+        <div className="flex flex-col gap-2 items-center text-secondary">
+          <div className="flex flex-row items-center gap-3">
+            <GiPadlock size={30} />
+            <h1 className="text-3xl font-semibold">Register</h1>
+          </div>
+          <p className="text-neutral-500">Welcome to Matchhy</p>
+        </div>
+      </CardHeader>
+      <CardBody>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="space-y-4">
+            <Input
+              defaultValue=""
+              label="Email"
+              variant="bordered"
+              {...register("email")}
+              isInvalid={!!errors.email}
+              errorMessage={errors.email?.message as string}
+            />
+            <Input
+              defaultValue=""
+              label="Name"
+              variant="bordered"
+              {...register("name")}
+              isInvalid={!!errors.name}
+              errorMessage={errors.name?.message as string}
+            />
+            <Input
+              defaultValue=""
+              label="Password"
+              type="password"
+              variant="bordered"
+              {...register("password")}
+              isInvalid={!!errors.password}
+              errorMessage={errors.password?.message as string}
+            />
+            {errors.root?.serverError && (
+              <p className="text-danger text-sm">
+                {errors.root?.serverError.message}
+              </p>
+            )}
+            <Button
+              disabled={!isValid}
+              isLoading={isSubmitting}
+              fullWidth
+              color="secondary"
+              type="submit"
+            >
+              {isValid ? <>Register</> : <>No no</>}
+            </Button>
+          </div>
+        </form>
+      </CardBody>
+    </Card>
+  );
+};
 
-export default RegisterForm
+export default RegisterForm;
--- app/actions/authActions.ts
+++ app/actions/authActions.ts
@@ -2,36 +2,57 @@
 
 import { prisma } from "@/lib/prisma";
 import { registerSchema, RegisterSchema } from "@/lib/registerSchema";
+import { ActionResult } from "@/types";
+import { User } from "@prisma/client";
 import bcryptjs from "bcryptjs";
-export async function registerUser(data: RegisterSchema) {
-  const validated = registerSchema.safeParse(data);
-  if (!validated.success) {
-    //throw new Error(validated.error.errors[0].message);
-    return { error: validated.error.errors };
-  }
+export async function registerUser(
+  data: RegisterSchema
+): Promise<ActionResult<User>> {
+  try {
+    const validated = registerSchema.safeParse(data);
 
-  const { name, email, password } = validated.data;
+    if (!validated.success) {
+      // throw new Error(validated.error.errors[0].message);
+      return {
+        status: "error",
+        error: validated.error.errors[0].message,
+      };
+      //return { error: validated.error.errors };
+    }
 
-  const hashedPassword = await bcryptjs.hash(password, 7);
+    const { name, email, password } = validated.data;
 
-  const existingUser = await prisma.user.findUnique({
-    where: {
-      email: email,
-    },
-  });
+    const hashedPassword = await bcryptjs.hash(password, 7);
 
-  if (existingUser) {
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (existingUser) {
+      return {
+        status: "error",
+        error: "User already exist",
+      };
+    }
+
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        passwordHash: hashedPassword,
+      },
+    });
     return {
-      error: "User already exist",
+      status: "success",
+      data: newUser,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      status: "error",
+      error: "Something went wrong",
     };
   }
-
-  const newUser = await prisma.user.create({
-    data: {
-      name,
-      email,
-      passwordHash: hashedPassword,
-    },
-  });
-  return newUser;
 }
