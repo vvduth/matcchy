d4625adc477366efc3bfbd6cee7c6b23b8287dae
vvduth <ducthai060501@gmail.com>
2025-04-14 13:02:52 +0300

    Middleware fix pt2

--- middleware.ts
+++ middleware.ts
@@ -1,42 +1,43 @@
 import {auth} from '@/auth';
- import {authRoutes, publicRoutes} from '@/routes';
- import {NextResponse} from 'next/server';
- 
- export default auth((req) => {
-     const {nextUrl} = req;
-     const isLoggedIn = !!req.auth;
- 
-     const isPublic = publicRoutes.includes(nextUrl.pathname);
-     const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-     const isProfileComplete = req.auth?.user.profileComplete
-     const isAdmin = req.auth?.user.role === 'ADMIN';
-     const isAdminRoute = nextUrl.pathname.startsWith('/admin');
- 
-     if (isPublic || isAdmin) {
-         return NextResponse.next();
-     }
-     if (isAdminRoute && !isAdmin) {
+import {authRoutes, publicRoutes} from '@/routes';
+import {NextResponse} from 'next/server';
+
+export default auth((req) => {
+    const {nextUrl} = req;
+    const isLoggedIn = !!req.auth;
+
+    const isPublic = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+    const isProfileComplete = req.auth?.user.profileComplete;
+    const isAdmin = req.auth?.user.role === 'ADMIN';
+    const isAdminRoute = nextUrl.pathname.startsWith('/admin');
+
+    if (isPublic || isAdmin) {
+        return NextResponse.next();
+    }
+
+    if (isAdminRoute && !isAdmin) {
         return NextResponse.redirect(new URL('/', nextUrl));
     }
-     if (isAuthRoute) {
-         if (isLoggedIn) {
-             return NextResponse.redirect(new URL('/members', nextUrl));
-         }
-         return NextResponse.next();
-     }
- 
-     if (!isPublic && !isLoggedIn) {
-         return NextResponse.redirect(new URL('/login', nextUrl));
-     }
 
-     // user logged in with social, still need profile provided
-     if (isLoggedIn && !isProfileComplete &&  !isAuthRoute && nextUrl.pathname !== '/complete-profile') {
-        return NextResponse.redirect(new URL('/complete-profile', nextUrl));
-     }
- 
-     return NextResponse.next();
- });
- 
- export const config = {
-     matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)']
+    if (isAuthRoute) {
+        if (isLoggedIn) {
+            return NextResponse.redirect(new URL('/members', nextUrl));
+        }
+        return NextResponse.next();
     }
+
+    if (!isPublic && !isLoggedIn) {
+        return NextResponse.redirect(new URL('/login', nextUrl));
+    }
+
+    if (isLoggedIn && !isProfileComplete && nextUrl.pathname !== '/complete-profile') {
+        return NextResponse.redirect(new URL('/complete-profile', nextUrl));
+    }
+
+    return NextResponse.next();
+});
+
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico|sitemap.xml|robots.txt).*)']
+}
